d67cab7a1d8890ed9a427c6e9dc86633f99bfc9a
Mahin-Hasan <mahinmtrs@gmail.com>
2024-10-11 01:30:25 +0600

    created new project for exploring more NEXT js concepts

---
+++ Next Reacp PHero/next-level-data-fetching/src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            this is the custom laoding page
+        </div>
+    );
+};
+
+export default loading;
---
+++ Next Reacp PHero/next-level-data-fetching/src/app/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen px-12 py-24">Next Level data fetcing</main>
+  );
+}
