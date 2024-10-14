e9a8ecd25a9f7b183c6172a03f957ef3e6209eba
Mahin-Hasan <mahinmtrs@gmail.com>
2024-10-15 02:48:36 +0600

    explored next js caching behaviour and explored revalidating

--- Next Reacp PHero/next-level-data-fetching/src/app/about/page.jsx
+++ Next Reacp PHero/next-level-data-fetching/src/app/about/page.jsx
@@ -7,9 +7,30 @@ export const metadata = {
   title: "About ",
   description: "Text generated from layout",
 };
+//no store solves the cached behavour in the front end
+// const getTime = async () => {
+//   const res = await fetch("http://localhost:3000/time", { cache: "no-store" });
+//   const data = await res.json();
+//   return data.currentTime;
+// };
+// update after a fixed time
+const getTime = async () => {
+  const res = await fetch("http://localhost:3000/time", {
+    next: { revalidate: 5 },
+  });
+  const data = await res.json();
+  return data.currentTime;
+};
 
-const AboutPage = () => {
-  return <div className={`${headland.className} min-h-screen px-12 py-24`}>This is about</div>;
+const AboutPage = async () => {
+  const currentTime = await getTime();
+
+  return (
+    <div className={`${headland.className} min-h-screen px-12 py-24`}>
+      <h1 className="text-2xl">This is about</h1>
+      <h2 className="text-xl text-blue-700">Time: {currentTime}</h2>
+    </div>
+  );
 };
 
 export default AboutPage;
