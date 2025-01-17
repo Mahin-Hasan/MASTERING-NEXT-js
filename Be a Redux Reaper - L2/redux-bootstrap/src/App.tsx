e37c4e5030bb407e16c014a0db445cf2080b08e4
Mahin-Hasan <mahinmtrs@gmail.com>
2025-01-18 02:41:02 +0600

    creted counter slice, actions and reducers

--- Be a Redux Reaper - L2/redux-bootstrap/src/App.tsx
+++ Be a Redux Reaper - L2/redux-bootstrap/src/App.tsx
@@ -1,13 +1,24 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment()); // must be called using () or else it will not work be careful
+  };
+  const handleDecrement = () => {
+    dispatch(decrement()); // must be called using () or else it will not work be careful
+  };
+
   return (
     <>
       <div>
         <h1>Counter with Redux</h1>
-        <button>Increment</button>
-        <div>0</div>
-        <button>Decrement</button>
+        <button onClick={handleIncrement}>Increment</button>
+        <div>s</div>
+        <button onClick={handleDecrement}>Decrement</button>
       </div>
     </>
   );
---
+++ Be a Redux Reaper - L2/redux-bootstrap/src/redux/features/counter/counterSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count = state.count + 1;
+    },
+    decrement: (state) => {
+      //   state.count = state.count - 1;
+      //   state.count--;
+      state.count -= 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions; // used as dispatch where this business logics are required
+export default counterSlice.reducer; // imported in store
